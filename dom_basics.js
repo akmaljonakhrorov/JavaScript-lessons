1fe5d1d663c43fb778ce874012afcae6eb46cc53
akmaljonakhrorov <117590078+akmaljonakhrorov@users.noreply.github.com>
2024-05-22 14:28:22 +0500

    Add files via upload

---
+++ dom_basics.js
@@ -0,0 +1,25 @@
+
+//  DOM 20 may, 2024 year.
+
+// https://t.me/c/1823828950/57
+// alert(window.innerHeight);
+
+
+// alert(document.href);
+// alert(location.pathname)
+
+// let data = document.getElementsByTagName('ul')[0];
+
+// console.log(data.parentNode);
+// console.log(data.parentElement);
+
+// console.log(data.firstChild);
+// console.log(data.firstElementChild);
+
+// tag ni ajratib olish va style berish
+
+let by = document.getElementsByTagName("body")
+let parent  = document.getElementsByClassName('parent');
+
+console.log((parent[0].style.background = "red"));
+console.log((parent[1].style.background = "orange"));
